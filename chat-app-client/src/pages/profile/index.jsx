96e30caafad230972c0f46ce7170b3448315899e
Tran Vu Binh <tranb4010@gmail.com>
2024-07-24 15:40:25 +0700

    test

--- chat-app-client/src/pages/profile/index.jsx
+++ chat-app-client/src/pages/profile/index.jsx
@@ -1,5 +1,5 @@
 import { useAppStore } from "@/store";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoArrowBack } from "react-icons/io5";
 import { AvatarImage, Avatar } from "@/components/ui/avatar";
@@ -9,6 +9,7 @@ import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { UPDATE_PROFILE_ROUTE } from "@/utils/constants";
 import apiClient from "@/lib/api-client";
+import { ADD_PROFILE_IMAGE_ROUTE, HOST } from "../../utils/constants";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -18,12 +19,16 @@ const Profile = () => {
   const [image, setImage] = useState(null);
   const [hovered, setHovered] = useState(false);
   const [selectedColor, setSelectedColor] = useState(0);
+  const fileInputRef = useRef(null);
 
   useEffect(() => {
     if (userInfo.profileSetup) {
       setFirstName(userInfo.firstName);
       setLastName(userInfo.lastName);
       setSelectedColor(userInfo.color);
+    }
+    if(userInfo.image) {
+      setImage(`${HOST}/${userInfo.image}`);
     }
   }, [userInfo]);
 
@@ -63,10 +68,44 @@ const Profile = () => {
     }
   };
 
+  const handleNavigate = () => {
+    if (userInfo.profileSetup) {
+      navigate("/");
+    } else {
+      toast.error("Please complete your profile setup");
+    }
+  };
+
+  const handleFileInputClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const hanldeImageChange = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("profile-image", file);
+      const response = await apiClient.post(ADD_PROFILE_IMAGE_ROUTE, formData, {
+        withCredentials: true,
+      });
+      if (response.status === 201 && response.data.image) {
+        setUserInfo({ ...userInfo, image: response.data.image });
+        toast.success("Image uploaded successfully");
+      }
+      // const reader = new FileReader();
+      // reader.onload = () => {
+      //   setImage(reader.result);
+      // };
+      // reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDeleteImage = async () => {};
+
   return (
     <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
       <div className=" flex flex-col gap-10 w-[80vw] md:w-max">
-        <div>
+        <div onClick={handleNavigate}>
           <IoArrowBack className="text-4xl lg:text-6xl text-white/90" />
         </div>
         <div className="grid grid-cols-2">
@@ -95,7 +134,10 @@ const Profile = () => {
               )}
             </Avatar>
             {hovered && (
-              <div className="absolute h-32 w-32 md:w-48 md:h-48  flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full">
+              <div
+                className="absolute h-32 w-32 md:w-48 md:h-48  flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full"
+                onClick={image ? handleDeleteImage : handleFileInputClick}
+              >
                 {image ? (
                   <FaTrash className="text-white text-3xl cursor-pointer" />
                 ) : (
@@ -103,7 +145,14 @@ const Profile = () => {
                 )}
               </div>
             )}
-            {/* <input type="text"></input> */}
+            <input
+              type="file"
+              ref={fileInputRef}
+              className="hidden"
+              onChange={hanldeImageChange}
+              name="profile-image"
+              accept=".png, .jpg, .jpeg, .svg, .webp"
+            ></input>
           </div>
           <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
             <div className="w-full">
@@ -150,7 +199,10 @@ const Profile = () => {
           </div>
         </div>
         <div className="w-full">
-          <Button className="h-16 w-full bg-purple-500 hover:bg-purple-900 cursor-pointer transition-all duration-300 text-lg" onClick={saveChanges}>
+          <Button
+            className="h-16 w-full bg-purple-500 hover:bg-purple-900 cursor-pointer transition-all duration-300 text-lg"
+            onClick={saveChanges}
+          >
             Save Changes
           </Button>
         </div>
